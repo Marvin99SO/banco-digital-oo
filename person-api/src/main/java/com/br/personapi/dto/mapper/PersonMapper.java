53267dfab710f62ea67260b511c6b39cab9f2ff7
Marcus <marcusvsdo@gmail.com>
2022-03-05 20:15:43 -0300

    Aplicando correções e atualizações

---
+++ person-api/src/main/java/com/br/personapi/dto/mapper/PersonMapper.java
@@ -0,0 +1,15 @@
+package com.br.personapi.dto.mapper;
+
+import com.br.personapi.dto.request.PersonDTO;
+import com.br.personapi.entities.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PersonMapper {
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO dto);
+
+    PersonDTO toDTO(Person dto);
+}
